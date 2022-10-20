2b1234796c16b10abb1f582023cf8faf00db5fd1
Patrick Piotrowski <106777856+patraopatra@users.noreply.github.com>
2022-10-20 15:23:56 -0300

    Upload de arquivo javascript
    Arquivo .js com as funções para armazenar o token.

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function voltar() {
+  window.history.back();
+}
+
+function teste() {
+  fetch('https://reqres.in/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: document.querySelector('.cemail').value,
+      password: document.querySelector('.cpassword').value
+    })
+  }).then(res => {
+    return res.json()
+  })
+    .then(data => {
+      console.log(data);
+      localStorage.setItem("token", data.token);
+    })
+    .catch(error => console.log('ERROR'))
+} 
